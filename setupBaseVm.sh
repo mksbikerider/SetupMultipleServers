fe50edac29c7d9ecd74dd40e84f06b2533e61754
auto <auto@DevCent.(none)>
2015-01-01 13:46:56 +0800

    Inital setup scripts

---
+++ setupBaseVm.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+sed 's/ONBOOT=no/ONBOOT=yes/' /etc/sysconfig/network-scripts/ifcfg-eth0
+sed 's/BOOTPROTO=dhcp/BOOTPROTO=no/' /etc/sysconfig/network-scripts/ifcfg-eth0
+sed 's/ONBOOT=no/ONBOOT=yes/' /etc/sysconfig/network-scripts/ifcfg-eth1
+
+
+
+iptables -F
+iptables -A INPUT  -p tcp --dport 22 -j ACCEPT
+
+iptables -P INPUT DROP
+iptables -P FORWARD DROP
+iptables -P OUTPUT ACCEPT
+
+iptables -A INPUT -i lo -j ACCEPT
+
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+
+service iptables save
+service iptables restart
+
+vi /etc/ssh/sshd_config
+sed 's/#PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config
+
+service network restart
+service sshd restart
